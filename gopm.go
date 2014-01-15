2d42d1e584120eede3b0436c3a8016ff16d9b069
Lunny Xiao <xiaolunwen@gmail.com>
2014-01-15 13:57:13 +0800

    version

--- gopm.go
+++ gopm.go
@@ -29,7 +29,7 @@ import (
 // Test that go1.1 tag above is included in builds. main.go refers to this definition.
 const go11tag = true
 
-const APP_VER = "0.6.2.0102"
+const APP_VER = "0.6.2.0115"
 
 // 	//cmd.CmdSearch,
 // 		cmdClean,
