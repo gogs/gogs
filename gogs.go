dc99c92462173103c50ac6a59ddf504eaa547bcd
0xBAADF00D <meyer.thibault@gmail.com>
2014-02-12 14:54:09 -0500

    Init commit

--- gogs.go
+++ gogs.go
@@ -5,15 +5,37 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/codegangsta/martini"
+	"github.com/martini-contrib/render"
 
 	"github.com/gogits/gogs/routers"
+	"github.com/gogits/gogs/utils"
+	"github.com/gogits/gogs/utils/log"
 )
 
 const APP_VER = "0.0.0.0212"
 
+func init() {
+
+}
+
 func main() {
+	log.Info("App Name: %s", utils.Cfg.MustValue("", "APP_NAME"))
+
 	m := martini.Classic()
-	m.Get("/", routers.HomeGet)
-	m.Run()
+
+	// Middleware.
+	m.Use(render.Renderer())
+
+	// Routers.
+	m.Get("/", routers.Home)
+
+	listenAddr := fmt.Sprintf("%s:%s",
+		utils.Cfg.MustValue("server", "HTTP_ADDR"),
+		utils.Cfg.MustValue("server", "HTTP_PORT", "3000"))
+	log.Info("Listen: %s", listenAddr)
+	http.ListenAndServe(listenAddr, m)
 }
