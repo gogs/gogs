ffe0ba562c2d749ea904e7171b424ae251c1f62e
FuXiaoHei <fuxiaohei@hexiaz.com>
2014-03-02 21:47:55 +0800

    ui for user/profile page, only skeleton

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+var Gogits = {};
+
+(function($){
+    Gogits.showTooltips = function(){
+        $("body").tooltip({
+            selector: "[data-toggle=tooltip]"
+            //container: "body"
+        });
+    };
+    Gogits.showTab = function (selector, index) {
+        if (!index) {
+            index = 0;
+        }
+        $(selector).tab("show");
+        $(selector).find("li:eq(" + index + ") a").tab("show");
+    }
+})(jQuery);
