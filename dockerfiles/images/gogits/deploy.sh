d43f8738a48b8740a292096ffdfab2dfd1f9e227
0xBAADF00D <meyer.thibault@gmail.com>
2014-04-11 14:55:43 +0800

    Add the auto build scripts for deplying gogs with Docker.

--- dockerfiles/images/gogits/deploy.sh
+++ dockerfiles/images/gogits/deploy.sh
@@ -1,15 +1,20 @@
-# deploy.sh in gogits image
-# Script in the gogits image
+# deploy.sh in gogits image, replace the configs and run gogs
+
 ## Replace the mysql password
-MYSQL_PASSWORD=kuajie8402
+MYSQL_PASSWORD=THE_MYSQL_PASSWORD
 MYSQL_ALIAS=DB
 MYSQL_PASSWORD_LINE=`awk '$0 ~ str{print NR+1}' str="USER = root" $GOPATH/src/github.com/gogits/gogs/conf/app.ini`
 
-sed -e "${MYSQL_PASSWORD_LINE}s/.*$/PASSWD = $MYSQL_PASSWORD/g" conf/app.ini 
+sed -i "${MYSQL_PASSWORD_LINE}s/.*$/PASSWD = $MYSQL_PASSWORD/g" $GOPATH/src/github.com/gogits/gogs/conf/app.ini 
 
 ## Replace the mysql address and port
+# When using --link in docker run, the mysql image's info looks like this:
 # DB_PORT=tcp://172.17.0.2:3306
 # DB_PORT_3306_TCP_PORT=3306
 # DB_PORT_3306_TCP_PROTO=tcp
-sed -e "/HOST = 127.0.0.1:3306/c\HOST = ${MYSQLALIAS}_PORT" app.ini
+# DB_PORT_3306_TCP_ADDR=172.17.0.2
+sed -i "/HOST = 127.0.0.1:3306/c\HOST = $DB_PORT_3306_TCP_ADDR:$DB_PORT_3306_TCP_PORT" $GOPATH/src/github.com/gogits/gogs/conf/app.ini
+cd $GOPATH/src/github.com/gogits/gogs/ 
 
+# The sudo is a must here, or the go within docker container won't get the current user by os.Getenv("USERNAME")
+sudo ./gogs web
