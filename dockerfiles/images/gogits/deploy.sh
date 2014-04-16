a9dca08797c1e71dddd8a21aae89f120e48090eb
0xBAADF00D <meyer.thibault@gmail.com>
2014-04-15 23:34:58 -0400

    Remove the installation of PostgreSQL, which not needed actually.

--- dockerfiles/images/gogits/deploy.sh
+++ dockerfiles/images/gogits/deploy.sh
@@ -10,16 +10,6 @@ DB_PASSWORD_LINE=`awk '$0 ~ str{print NR+1}' str="USER = root" $GOPATH/src/githu
 sed -i "${DB_TYPE_LINE}s/.*$/DB_TYPE = $DB_TYPE/g" $GOPATH/src/github.com/gogits/gogs/conf/app.ini 
 sed -i "${DB_PASSWORD_LINE}s/.*$/PASSWD = $DB_PASSWORD/g" $GOPATH/src/github.com/gogits/gogs/conf/app.ini 
 
-if [ $DB_TYPE = "postgres" ]
-  then
-  # Add the postgres in gogs image.
-  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8
-  echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" > /etc/apt/sources.list.d/pgdg.list
-  apt-get update
-  apt-get -y -q install python-software-properties software-properties-common
-  apt-get -y -q install postgresql-9.3 postgresql-client-9.3 postgresql-contrib-9.3
-fi
-
 ## Replace the database address and port
 # When using --link in docker run, the database image's info looks like this:
 # DB_PORT=tcp://172.17.0.2:3306
