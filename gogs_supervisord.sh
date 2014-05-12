11ceff0f2115b1d6742ff5b1f872999b82235e2a
0xBAADF00D <meyer.thibault@gmail.com>
2014-05-12 13:20:13 +0800

    add supervisor support and script, easy to use.
    just like ./gogs_supervisord.sh start
    ./gogs_supervisord.sh stop
    ./gogs_supervisord.sh restart
    and logs all saved tmp folder

---
+++ gogs_supervisord.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+PID="tmp/supervisord.pid"
+CONF="etc/supervisord.conf"
+
+stop() {
+    if [ -f $PID ]; then
+        kill `cat -- $PID`
+        rm -f -- $PID
+        echo "stopped"
+    fi
+}
+
+start() {
+    echo "starting"
+    if [ ! -f $PID ]; then
+        supervisord -c $CONF
+        echo "started"
+    fi
+}
+
+case "$1" in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    restart)
+        stop
+        start
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|restart}"
+esac
