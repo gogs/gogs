cd0ddcfa01ea06ff64e4a05032f718a4b3097c25
Marios Andreopoulos <opensource@andmarios.com>
2014-08-25 20:35:23 +0300

    fix typo

--- routers/user/setting.go
+++ routers/user/setting.go
@@ -172,7 +172,7 @@ func SettingsSSHKeysPost(ctx *middleware.Context, form auth.AddSSHKeyForm) {
 		}
 
 		// Remove newline characters from form.KeyContent
-		cleanContent := strings.Replace(form.Content, "\n", "", -1)
+		cleanContent := string.Replace(form.Content, "\n", "", -1)
 
 		if ok, err := models.CheckPublicKeyString(cleanContent); !ok {
 			ctx.Flash.Error(ctx.Tr("form.invalid_ssh_key", err.Error()))
