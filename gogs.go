9bc63bda7ded31b77ad4aaf3495582a7c8555d2f
Unknwon <joe2010xtmf@163.com>
2014-08-30 20:49:51 +0800

    Finish all new admin pages.

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.4.8.0829 Alpha"
+const APP_VER = "0.4.9.0830 Alpha"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
