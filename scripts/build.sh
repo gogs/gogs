f67c59135fe258a9e839af7f839770b445d58732
lunnyxiao <xiaolunwen@gmail.com>
2014-09-12 14:45:38 +0800

    build script fixed

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,21 @@
 rm -rf output
 mkdir output
-go build
+go build ../gogs.go
 chmod +x gogs
 mv gogs ./output/
-cp -r ./conf/ ./output/conf/
-cp -r ./custom/ ./output/custom/
+cp -r ../conf/ ./output/conf/
+cp -r ../custom/ ./output/custom/
 cp -r ./dockerfiles/ ./output/dockerfiles/
-cp -r ./public/ ./output/public/
-cp -r ./templates/ ./output/templates/
-cp bee.json ./output/
-cp cert.pem ./output/
-cp CONTRIBUTING.md ./output/
+cp -r ../public/ ./output/public/
+cp -r ../templates/ ./output/templates/
+cp ../cert.pem ./output/
+cp ../CONTRIBUTING.md ./output/
 cp gogs_supervisord.sh ./output/
-cp key.pem ./output/
-cp LICENSE ./output/
-cp README.md ./output/
-cp README_ZH.md ./output/
-cp rpp.ini ./output/
+cp ../key.pem ./output/
+cp ../LICENSE ./output/
+cp ../README.md ./output/
+cp ../README_ZH.md ./output/
 cp start.bat ./output/
 cp start.sh ./output/
-cp wercker.yml ./output/
+cp ../wercker.yml ./output/
+cp mysql.sql ./output/
