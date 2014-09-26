4a1b5e9f8e2cc6eaf0e1a906ae0502922157c97c
0xBAADF00D <meyer.thibault@gmail.com>
2014-09-25 20:55:14 -0400

    Mirror template bug fix

--- routers/repo/commit.go
+++ routers/repo/commit.go
@@ -96,9 +96,10 @@ func SearchCommits(ctx *middleware.Context) {
 
 	commits, err := ctx.Repo.Commit.SearchCommits(keyword)
 	if err != nil {
-		ctx.Handle(500, "repo.SearchCommits(SearchCommits)", err)
+		ctx.Handle(500, "SearchCommits", err)
 		return
 	}
+	commits = models.ValidCommitsWithEmails(commits)
 
 	ctx.Data["Keyword"] = keyword
 	ctx.Data["Username"] = userName
