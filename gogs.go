3a28c8dd14c1f8dd363efea15e3cdf553de79d12
0xBAADF00D <meyer.thibault@gmail.com>
2014-11-02 10:57:29 -0500

    Update gopmfile

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.5.6.1027 Beta"
+const APP_VER = "0.5.6.1102 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
