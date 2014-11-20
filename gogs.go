0c92da7e9c4b4f41c016d14079db2ee405dbffba
Unknwon <joe2010xtmf@163.com>
2014-11-19 21:19:37 -0500

    fix invalid user links in admin panels

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.5.8.1118 Beta"
+const APP_VER = "0.5.8.1119 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
