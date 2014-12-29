63c1f9a23fd9b3d756e6763005df9262ccb15101
Unknwon <joe2010xtmf@163.com>
2014-12-29 20:00:07 +0800

    fix 500: E-mail already used in user settings page

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.5.10.1222 Beta"
+const APP_VER = "0.5.10.1229 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
