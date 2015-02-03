98f7f6bef8ad9687324fd72ef115f5d2d1e5c34a
Unknwon <joe2010xtmf@163.com>
2015-02-03 12:42:34 -0500

    scripts: remove confuse scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a MIT-style
 # license that can be found in the LICENSE file.
 #
-# start gogs web
+# MUST EXECUTE THIS AT ROOT DIRECTORY: ./scripts/start.sh
 #
 IFS='
 	'
