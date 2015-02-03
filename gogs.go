8fa64bf149b278b5679a72397b704320093a448c
0xBAADF00D <meyer.thibault@gmail.com>
2015-02-02 23:04:36 -0500

    modules/base: fix README filename checker, fix #877

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.5.12.0201 Beta"
+const APP_VER = "0.5.12.0202 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
