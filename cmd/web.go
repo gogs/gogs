4dfffee9fb716672c7170013a4f69ca759f2dd51
Unknwon <joe2010xtmf@163.com>
2015-02-04 19:14:09 -0500

    cmd: update csrf version requirement

--- cmd/web.go
+++ cmd/web.go
@@ -80,7 +80,7 @@ func checkVersion() {
 		{"github.com/Unknwon/macaron", macaron.Version, "0.5.1"},
 		{"github.com/macaron-contrib/binding", binding.Version, "0.0.4"},
 		{"github.com/macaron-contrib/cache", cache.Version, "0.0.7"},
-		{"github.com/macaron-contrib/csrf", csrf.Version, "0.0.1"},
+		{"github.com/macaron-contrib/csrf", csrf.Version, "0.0.3"},
 		{"github.com/macaron-contrib/i18n", i18n.Version, "0.0.5"},
 		{"github.com/macaron-contrib/session", session.Version, "0.1.6"},
 		{"gopkg.in/ini.v1", ini.Version, "1.2.0"},
