b1a56293944f4b94749c54201c9ab5146641786d
0xBAADF00D <meyer.thibault@gmail.com>
2015-06-05 19:52:00 +0200

    Add CommonName to the self signed certificate
    Without a CN the self signed certificate is considered
    invalid by chrome.
    You can check with: openssl x509 -in cert.pem -subject -noout

--- cmd/cert.go
+++ cmd/cert.go
@@ -114,6 +114,7 @@ func runCert(ctx *cli.Context) {
 		SerialNumber: serialNumber,
 		Subject: pkix.Name{
 			Organization: []string{"Acme Co"},
+			CommonName: "Gogs",
 		},
 		NotBefore: notBefore,
 		NotAfter:  notAfter,
