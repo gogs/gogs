35be8741c4523206aa3bff4aad1dda23847b381e
Unknwon <u@gogs.io>
2015-07-10 16:51:25 +0800

    fix #1112

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.6.1.0709 Beta"
+const APP_VER = "0.6.1.0710 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
