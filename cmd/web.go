984d18fe5756b2d9721981f0405ddce89089cba6
Unknwon <u@gogs.io>
2015-07-28 18:01:27 +0800

    fix #1305

--- cmd/web.go
+++ cmd/web.go
@@ -85,7 +85,7 @@ func checkVersion() {
 		{"github.com/macaron-contrib/csrf", csrf.Version, "0.0.3"},
 		{"github.com/macaron-contrib/i18n", i18n.Version, "0.0.7"},
 		{"github.com/macaron-contrib/session", session.Version, "0.1.6"},
-		{"gopkg.in/ini.v1", ini.Version, "1.2.0"},
+		{"gopkg.in/ini.v1", ini.Version, "1.3.4"},
 	}
 	for _, c := range checkers {
 		ver := strings.Join(strings.Split(c.Version(), ".")[:3], ".")
