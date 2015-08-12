ab0247b4dc0b968a68bbb75c97d5fe022cca427a
Don Bowman <don.waterloo@gmail.com>
2015-08-12 22:00:43 +0000

    race condition on keydelete

--- models/publickey.go
+++ models/publickey.go
@@ -386,8 +386,6 @@ func ListPublicKeys(uid int64) ([]*PublicKey, error) {
 
 // rewriteAuthorizedKeys finds and deletes corresponding line in authorized_keys file.
 func rewriteAuthorizedKeys(key *PublicKey, p, tmpP string) error {
-	sshOpLocker.Lock()
-	defer sshOpLocker.Unlock()
 
 	fr, err := os.Open(p)
 	if err != nil {
@@ -444,6 +442,9 @@ func UpdatePublicKey(key *PublicKey) error {
 }
 
 func deletePublicKey(e *xorm.Session, key *PublicKey) error {
+	sshOpLocker.Lock()
+	defer sshOpLocker.Unlock()
+
 	has, err := e.Get(key)
 	if err != nil {
 		return err
