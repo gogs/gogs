38eb000d13d5f082e45da4629914fb3d8e633379
Unknwon <u@gogs.io>
2015-09-03 08:09:08 -0400

    work on #1570

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.6.8.0902 Beta"
+const APP_VER = "0.6.9.0903 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
