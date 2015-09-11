373ef5d15e1c2781bf47750554cb21e4ed5446c1
Unknwon <u@gogs.io>
2015-09-11 12:33:27 -0400

    #1500 timezone minutes offset

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.6.10.0910 Beta"
+const APP_VER = "0.6.11.0911 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
