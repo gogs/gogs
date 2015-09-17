5bcd70e58cb4b8807220c5bcccb1cfb4588535b7
Unknwon <u@gogs.io>
2015-09-17 15:45:36 -0400

    finish all new template for current ones

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.6.12.0916 Beta"
+const APP_VER = "0.6.13.0917 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
