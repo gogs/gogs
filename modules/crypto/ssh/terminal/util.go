e07675b4806b306606769e8534934d86dd7c7f10
Unknwon <u@gogs.io>
2015-09-24 18:03:08 -0400

    fix test custom import path

--- modules/crypto/ssh/terminal/util.go
+++ modules/crypto/ssh/terminal/util.go
@@ -14,7 +14,7 @@
 // 	        panic(err)
 // 	}
 // 	defer terminal.Restore(0, oldState)
-package terminal // import "golang.org/x/crypto/ssh/terminal"
+package terminal
 
 import (
 	"io"
--- modules/crypto/ssh/testdata/doc.go
+++ modules/crypto/ssh/testdata/doc.go
@@ -5,4 +5,4 @@
 // This package contains test data shared between the various subpackages of
 // the golang.org/x/crypto/ssh package. Under no circumstance should
 // this data be used for production code.
-package testdata // import "golang.org/x/crypto/ssh/testdata"
+package testdata
