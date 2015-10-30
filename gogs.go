706b0f72e2e4bc91bdbed38fee609cabe9f44e43
Unknwon <u@gogs.io>
2015-10-29 20:40:57 -0400

    fix issue comment mention and autofix count when start

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.6.18.1026 Beta"
+const APP_VER = "0.6.18.1029 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
