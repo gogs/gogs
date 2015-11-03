25ec20d5251511ebd0b9e6b963e189b860c39704
Unknwon <u@gogs.io>
2015-11-03 17:25:39 -0500

    #1838 update merge base before generate new patch

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.6.22.1103 Beta"
+const APP_VER = "0.6.23.1103 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
