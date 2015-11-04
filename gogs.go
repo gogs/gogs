2d37f6b279f97b0c8e5ff414bd5f0e7da43dbfa7
0xBAADF00D <meyer.thibault@gmail.com>
2015-11-04 10:16:50 -0500

    fix #1876

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.6.24.1103 Beta"
+const APP_VER = "0.6.24.1104 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
