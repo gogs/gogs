bec3bc723aeef42d30557dfdd0c2a305b7db3d96
Unknwon <u@gogs.io>
2015-11-06 11:10:27 -0500

    #1433 images with links in Markdown
    - #1904 minor fix on image link

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.6.27.1105 Beta"
+const APP_VER = "0.6.28.1106 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
