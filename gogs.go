48fcbd6340c6bf86031e6d6fee8cf33b4caafcd7
Unknwon <u@gogs.io>
2015-11-08 11:08:13 -0500

    prepare 0.7.0

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.6.28.1107 Beta"
+const APP_VER = "0.7.0.1107 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
