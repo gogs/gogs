1780625af6028e1781bdd85b238fc810fef74350
0xBAADF00D <meyer.thibault@gmail.com>
2015-11-12 12:32:40 -0500

    fix #1930

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.7.5.1111 Beta"
+const APP_VER = "0.7.6.1112 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
