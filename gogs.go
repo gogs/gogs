c9261ea4f4a164b883b4f52981105699fc7d891b
Unknwon <u@gogs.io>
2015-11-16 00:16:03 -0500

    UI fix

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.7.12.1115 Beta"
+const APP_VER = "0.7.12.1116 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
