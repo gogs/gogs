44b4eb5e901e763d9a45f12b2d35e7e65178ac75
Unknwon <u@gogs.io>
2015-11-18 19:32:23 -0500

    #1742 Update default branch in git repository while change in web view

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.7.16.1118 Beta"
+const APP_VER = "0.7.17.1118 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
