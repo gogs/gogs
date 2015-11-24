0128036514ed40b2c878053e633762a8cffed74d
Unknwon <u@gogs.io>
2015-11-23 22:32:07 -0500

    #1681 some fixes for builtin SSH server on Windows

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.7.20.1121 Beta"
+const APP_VER = "0.7.21.1123 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
