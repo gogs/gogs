4d9499c2d39d9978c611ab8c3a4c6fea6bbdf0de
Unknwon <u@gogs.io>
2015-12-03 18:10:45 -0500

    fix broken link

--- routers/repo/setting.go
+++ routers/repo/setting.go
@@ -257,7 +257,7 @@ func Webhooks(ctx *middleware.Context) {
 	ctx.Data["Title"] = ctx.Tr("repo.settings.hooks")
 	ctx.Data["PageIsSettingsHooks"] = true
 	ctx.Data["BaseLink"] = ctx.Repo.RepoLink
-	ctx.Data["Description"] = ctx.Tr("repo.settings.hooks_desc", "https://github.com/gogits/go-gogs-client/wiki/Repositories---Webhooks")
+	ctx.Data["Description"] = ctx.Tr("repo.settings.hooks_desc", "https://github.com/gogits/go-gogs-client/wiki/Repositories-Webhooks")
 
 	ws, err := models.GetWebhooksByRepoID(ctx.Repo.Repository.ID)
 	if err != nil {
