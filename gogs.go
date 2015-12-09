d97809d3e2849a6673fc9b97c636ef7a99c983b7
Unknwon <u@gogs.io>
2015-12-09 11:38:12 -0500

    #1943 Able to config fsck timeout

--- gogs.go
+++ gogs.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.7.35.1209 Beta"
+const APP_VER = "0.7.36.1209 Beta"
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
