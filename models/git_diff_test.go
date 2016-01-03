fed9df92aabbc0c4515a78124fdfff9b3f71e54f
0xBAADF00D <meyer.thibault@gmail.com>
2016-01-03 19:26:46 -0200

    Highlighting differences of lines in the diff view.

---
+++ models/git_diff_test.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+  dmp "github.com/sergi/go-diff/diffmatchpatch"
+  "html/template"
+  "testing"
+)
+
+func assertEqual(t *testing.T, s1 string, s2 template.HTML) {
+  if s1 != string(s2) {
+    t.Errorf("%s should be equal %s", s2, s1)
+  }
+}
+
+func TestDiffToHtml(t *testing.T) {
+  assertEqual(t, "foo <span class=\"added-code\">bar</span> biz", diffToHtml([]dmp.Diff{
+    dmp.Diff{dmp.DiffEqual, "foo "},
+    dmp.Diff{dmp.DiffInsert, "bar"},
+    dmp.Diff{dmp.DiffDelete, " baz"},
+    dmp.Diff{dmp.DiffEqual, " biz"},
+  }, DIFF_LINE_ADD))
+
+  assertEqual(t, "foo <span class=\"removed-code\">bar</span> biz", diffToHtml([]dmp.Diff{
+    dmp.Diff{dmp.DiffEqual, "foo "},
+    dmp.Diff{dmp.DiffDelete, "bar"},
+    dmp.Diff{dmp.DiffInsert, " baz"},
+    dmp.Diff{dmp.DiffEqual, " biz"},
+  }, DIFF_LINE_DEL))
+}
