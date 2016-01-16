92798f5ebee302f56ce9aff26fe143ed0e09724d
Antoine GIRARD <sapk@sapk.fr>
2016-01-16 10:36:16 +0100

    Handling error for the API request and add commments [ci skip]

--- routers/api/v1/repo/branch.go
+++ routers/api/v1/repo/branch.go
@@ -14,36 +14,42 @@ import (
 // Temporary: https://gist.github.com/sapk/df64347ff218baf4a277#get-a-branch
 // https://github.com/gogits/go-gogs-client/wiki/Repositories-Branches#get-a-branch
 func GetBranch(ctx *middleware.Context) {
+	//Getting the branch requested
 	branch, err := ctx.Repo.Repository.GetBranch(ctx.Params(":id"))
 	if err != nil {
-		//TODO handle error
+		ctx.APIError(500, "Repository.GetBranch", err)
 		return
 	}
+	//Getting the last commit of the branch
 	c, err := branch.GetCommit()
 	if err != nil {
-		//TODO handle error
+		ctx.APIError(500, "Branch.GetCommit", err)
 		return
 	}
+	//Converting to API format and send payload
 	ctx.JSON(200, convert.ToApiBranch(branch,c))
 }
 
 // Temporary: https://gist.github.com/sapk/df64347ff218baf4a277#list-branches
 // https://github.com/gogits/go-gogs-client/wiki/Repositories-Branches#list-branches
 func ListBranches(ctx *middleware.Context) {
+	//Listing of branches
 	Branches, err := ctx.Repo.Repository.GetBranches()
 	if err != nil {
-		//TODO handle error
+		ctx.APIError(500, "Repository.GetBranches", err)
 		return
 	}
+	//Getting the last commit of each branch
 	apiBranches := make([]*api.Branch, len(Branches))
 	for i := range Branches {
 		c, err := Branches[i].GetCommit()
 		if err != nil {
-			//TODO handle error
-			continue
+			ctx.APIError(500, "Branch.GetCommit", err)
+			return
 		}
+		//Converting to API format
 		apiBranches[i] = convert.ToApiBranch(Branches[i],c)
 	}
-
+	//Sending the payload
 	ctx.JSON(200, &apiBranches)
 }
