61cb6b1f8d555ffce216245b98bd56b80629b9df
Faraz Fazli <farazfazli@gmail.com>
2016-01-18 22:25:38 -0500

    Update copyrights

--- routers/api/v1/repo/keys.go
+++ routers/api/v1/repo/keys.go
@@ -1,4 +1,4 @@
-// Copyright 2015 The Gogs Authors. All rights reserved.
+// Copyright 2016 The Gogs Authors. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
