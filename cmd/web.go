9e89584cb4aa6321311dd291c364c5d956fcead1
Unknwon <u@gogs.io>
2016-02-29 19:29:49 -0500

    Allow setting git operations timeouts
    - Migrate: #2704 #2653
    - Clone: #2701
    - Mirror, Pull

--- cmd/web.go
+++ cmd/web.go
@@ -88,7 +88,7 @@ func checkVersion() {
 		{"github.com/go-macaron/toolbox", toolbox.Version, "0.1.0"},
 		{"gopkg.in/ini.v1", ini.Version, "1.8.4"},
 		{"gopkg.in/macaron.v1", macaron.Version, "0.8.0"},
-		{"github.com/gogits/git-module", git.Version, "0.2.7"},
+		{"github.com/gogits/git-module", git.Version, "0.2.8"},
 		{"github.com/gogits/go-gogs-client", gogs.Version, "0.7.3"},
 	}
 	for _, c := range checkers {
