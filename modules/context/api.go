d8f27ecbf6da9c4aaa4904ea1f9a25154eeac3c2
Unknwon <u@gogs.io>
2016-03-13 17:37:44 -0400

    Add APIContext

---
+++ modules/context/api.go
@@ -0,0 +1,22 @@
+// Copyright 2016 The Gogs Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package context
+
+import (
+	"gopkg.in/macaron.v1"
+)
+
+type APIContext struct {
+	*Context
+}
+
+func APIContexter() macaron.Handler {
+	return func(c *Context) {
+		ctx := &APIContext{
+			Context: c,
+		}
+		c.Map(ctx)
+	}
+}
