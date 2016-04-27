b2510312306ea9c2ea39f9488986a81d3a6a1a35
Kim Carlbäcker <kim.carlbacker@gmail.com>
2016-04-27 03:48:44 +0200

    PDF-Previews in file-lists now working (#3000)

--- routers/repo/view.go
+++ routers/repo/view.go
@@ -70,7 +70,6 @@ func Home(ctx *context.Context) {
 		ctx.Handle(404, "repo.Home", nil)
 		return
 	}
-
 	if entry != nil && !entry.IsDir() {
 		blob := entry.Blob()
 
@@ -92,9 +91,12 @@ func Home(ctx *context.Context) {
 
 			_, isTextFile := base.IsTextFile(buf)
 			_, isImageFile := base.IsImageFile(buf)
+			_, isPDFFile := base.IsPDFFile(buf)
 			ctx.Data["IsFileText"] = isTextFile
 
 			switch {
+			case isPDFFile:
+				ctx.Data["IsPDFFile"] = true
 			case isImageFile:
 				ctx.Data["IsImageFile"] = true
 			case isTextFile:
