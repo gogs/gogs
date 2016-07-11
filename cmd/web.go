ce75074b240ac04c08f2c3a93c5a411c17fc57f4
0xBAADF00D <meyer.thibault@gmail.com>
2016-07-12 06:20:09 +0800

    #2586 bump git-module requirement

--- cmd/web.go
+++ cmd/web.go
@@ -87,7 +87,7 @@ func checkVersion() {
 		{"github.com/go-macaron/toolbox", toolbox.Version, "0.1.0"},
 		{"gopkg.in/ini.v1", ini.Version, "1.8.4"},
 		{"gopkg.in/macaron.v1", macaron.Version, "1.1.2"},
-		{"github.com/gogits/git-module", git.Version, "0.3.1"},
+		{"github.com/gogits/git-module", git.Version, "0.3.2"},
 		{"github.com/gogits/go-gogs-client", gogs.Version, "0.7.4"},
 	}
 	for _, c := range checkers {
