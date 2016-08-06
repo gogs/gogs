30fda0f1aecbb45b0abc67e0fb9fdb806a52cb8c
Thibault Meyer <0xbaadf00d@users.noreply.github.com>
2016-08-06 19:02:15 +0200

    Fix #3315: Release dont use tag creation date (#3374)
    * Fix #3315: Release dont use tag creation date
    
    * Simplify code and apply gofmt
    
    * remove useless block (ctx.Repo.GitRepo.GetTag) on EditReleasePost
    
    * apply gofmt on modified files

--- models/release.go
+++ models/release.go
@@ -39,7 +39,9 @@ type Release struct {
 }
 
 func (r *Release) BeforeInsert() {
-	r.CreatedUnix = time.Now().Unix()
+	if r.CreatedUnix == 0 {
+		r.CreatedUnix = time.Now().Unix()
+	}
 }
 
 func (r *Release) AfterSet(colName string, _ xorm.Cell) {
