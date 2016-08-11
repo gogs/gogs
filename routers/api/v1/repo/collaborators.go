7e9b42c87d2276dec2f1789e1296c2ac51533657
Unknwon <u@gogs.io>
2016-08-11 11:35:46 -0700

    #2780 code clean up

--- routers/api/v1/repo/collaborators.go
+++ routers/api/v1/repo/collaborators.go
@@ -1,4 +1,4 @@
-// Copyright 2014 The Gogs Authors. All rights reserved.
+// Copyright 2016 The Gogs Authors. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
@@ -13,7 +13,6 @@ import (
 
 func AddCollaborator(ctx *context.APIContext, form api.AddCollaboratorOption) {
 	collaborator, err := models.GetUserByName(ctx.Params(":collaborator"))
-
 	if err != nil {
 		if models.IsErrUserNotExist(err) {
 			ctx.Error(422, "", err)
@@ -28,22 +27,12 @@ func AddCollaborator(ctx *context.APIContext, form api.AddCollaboratorOption) {
 		return
 	}
 
-	mode := models.ACCESS_MODE_WRITE
-	if form.Permission != nil && *form.Permission == "pull" {
-		mode = models.ACCESS_MODE_READ
-	} else if form.Permission != nil && *form.Permission == "push" {
-		mode = models.ACCESS_MODE_WRITE
-	} else if form.Permission != nil && *form.Permission == "admin" {
-		mode = models.ACCESS_MODE_ADMIN
-	} else if form.Permission != nil {
-		ctx.Error(500, "Permission", "Invalid permission type")
-		return
-	}
-	if err := ctx.Repo.Repository.ChangeCollaborationAccessMode(collaborator.Id, mode); err != nil {
-		ctx.Error(500, "ChangeCollaborationAccessMode", err)
-		return
+	if form.Permission != nil {
+		if err := ctx.Repo.Repository.ChangeCollaborationAccessMode(collaborator.ID, models.ParseAccessMode(*form.Permission)); err != nil {
+			ctx.Error(500, "ChangeCollaborationAccessMode", err)
+			return
+		}
 	}
 
 	ctx.Status(204)
-	return
 }
