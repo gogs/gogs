4317748ceaa35b23c3172300d630d8de60bcf347
Thibault Meyer <meyer.thibault@gmail.com>
2016-08-15 11:07:54 +0200

    Avoid reading file from other directory (security issue)

--- routers/repo/issue.go
+++ routers/repo/issue.go
@@ -953,7 +953,7 @@ func Labels(ctx *context.Context) {
 }
 
 func getLabelTemplateFile(name string) ([]byte, error) {
-	relPath := path.Join("conf/label", name)
+	relPath := path.Join("conf/label", strings.Replace(name, ".", "", -1))
 
 	// Use custom file when available.
 	customPath := path.Join(setting.CustomPath, relPath)
