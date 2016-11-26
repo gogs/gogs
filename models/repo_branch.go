ce8c9ef58060c64894229d2f11aed98517e39fbf
Bwko <bouwko@gmail.com>
2016-11-26 01:40:35 +0100

    Lint models/repo_branch.go

--- models/repo_branch.go
+++ models/repo_branch.go
@@ -8,11 +8,13 @@ import (
 	"code.gitea.io/git"
 )
 
+// Branch holds the branch information
 type Branch struct {
 	Path string
 	Name string
 }
 
+// GetBranchesByPath returns a branch by it's path
 func GetBranchesByPath(path string) ([]*Branch, error) {
 	gitRepo, err := git.OpenRepository(path)
 	if err != nil {
@@ -34,24 +36,27 @@ func GetBranchesByPath(path string) ([]*Branch, error) {
 	return branches, nil
 }
 
-func (repo *Repository) GetBranch(br string) (*Branch, error) {
-	if !git.IsBranchExist(repo.RepoPath(), br) {
-		return nil, &ErrBranchNotExist{br}
+// GetBranch returns a branch by it's name
+func (repo *Repository) GetBranch(branch string) (*Branch, error) {
+	if !git.IsBranchExist(repo.RepoPath(), branch) {
+		return nil, &ErrBranchNotExist{branch}
 	}
 	return &Branch{
 		Path: repo.RepoPath(),
-		Name: br,
+		Name: branch,
 	}, nil
 }
 
+// GetBranches returns all the branches of a repository
 func (repo *Repository) GetBranches() ([]*Branch, error) {
 	return GetBranchesByPath(repo.RepoPath())
 }
 
-func (br *Branch) GetCommit() (*git.Commit, error) {
-	gitRepo, err := git.OpenRepository(br.Path)
+// GetCommit returns all the commits of a branch
+func (branch *Branch) GetCommit() (*git.Commit, error) {
+	gitRepo, err := git.OpenRepository(branch.Path)
 	if err != nil {
 		return nil, err
 	}
-	return gitRepo.GetBranchCommit(br.Name)
+	return gitRepo.GetBranchCommit(branch.Name)
 }
