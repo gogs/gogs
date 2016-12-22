3376354ed8a38c6a3c4f27d6a1111cfa8f739089
Stefan Grönke <stefan@gronke.org>
2016-12-22 05:49:56 +0100

    Check if file is a symlink with web editor (#3687)
    * editor checks for symlinks
    
    * translate file_is_a_symlink message
    
    * credit translation author

--- routers/repo/editor.go
+++ routers/repo/editor.go
@@ -184,6 +184,11 @@ func editFilePost(ctx *context.Context, form auth.EditRepoFileForm, isNewFile bo
 				return
 			}
 		} else {
+			if entry.IsLink() {
+				ctx.Data["Err_TreePath"] = true
+				ctx.RenderWithErr(ctx.Tr("repo.editor.file_is_a_symlink", part), EDIT_FILE, &form)
+				return
+			}
 			if entry.IsDir() {
 				ctx.Data["Err_TreePath"] = true
 				ctx.RenderWithErr(ctx.Tr("repo.editor.filename_is_a_directory", part), EDIT_FILE, &form)
