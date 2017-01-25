c0562193375b9fcdf595cd370b796c3f23758c61
Óscar García Amor <ogarcia@connectical.com>
2017-01-25 23:48:31 +0100

    Fix #3714 (#4060)

--- models/mail.go
+++ models/mail.go
@@ -160,7 +160,8 @@ func composeIssueMessage(issue *Issue, doer *User, tplName base.TplName, tos []s
 	if err != nil {
 		log.Error(3, "HTMLString (%s): %v", tplName, err)
 	}
-	msg := mailer.NewMessageFrom(tos, fmt.Sprintf(`"%s" <%s>`, doer.DisplayName(), setting.MailService.FromEmail), subject, content)
+	from := gomail.NewMessage().FormatAddress(setting.MailService.FromEmail, doer.DisplayName())
+	msg := mailer.NewMessageFrom(tos, from, subject, content)
 	msg.Info = fmt.Sprintf("Subject: %s, %s", subject, info)
 	return msg
 }
