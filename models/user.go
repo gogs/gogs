ec5a967937a8dd4fb5287591e8dedcdedfa2b32d
Renato P. de Aquino <renato.aquino@gmail.com>
2017-01-30 11:35:12 -0200

    Issue #3814 (#3903)

--- models/user.go
+++ models/user.go
@@ -469,6 +469,12 @@ func (u *User) ShortName(length int) string {
 	return base.EllipsisString(u.Name, length)
 }
 
+// IsMailable checks if a user is elegible
+// to receive emails.
+func (u *User) IsMailable() bool {
+	return u.IsActive
+}
+
 // IsUserExist checks if given user name exist,
 // the user name should be noncased unique.
 // If uid is presented, then check will rule out that one,
@@ -929,7 +935,9 @@ func GetUserEmailsByNames(names []string) []string {
 		if err != nil {
 			continue
 		}
-		mails = append(mails, u.Email)
+		if u.IsMailable() {
+			mails = append(mails, u.Email)
+		}
 	}
 	return mails
 }
