ec9c14c09d1003792955be0baba34d68944e2950
Unknwon <u@gogs.io>
2017-02-14 18:47:09 -0500

    vendor: update dependency

--- vendor/github.com/gogits/go-gogs-client/repo_collaborator.go
+++ vendor/github.com/gogits/go-gogs-client/repo_collaborator.go
@@ -10,8 +10,18 @@ import (
 	"fmt"
 )
 
+type Collaborator struct {
+	*User
+	Permissions Permission `json:"permissions"`
+}
+
 type AddCollaboratorOption struct {
 	Permission *string `json:"permission"`
+}
+
+func (c *Client) ListCollaborator(user, repo string) ([]*Collaborator, error) {
+	collabs := make([]*Collaborator, 0, 10)
+	return collabs, c.getParsedResponse("GET", fmt.Sprintf("/repos/%s/%s/collaborators", user, repo), nil, nil, &collabs)
 }
 
 func (c *Client) AddCollaborator(user, repo, collaborator string, opt AddCollaboratorOption) error {
@@ -20,5 +30,15 @@ func (c *Client) AddCollaborator(user, repo, collaborator string, opt AddCollabo
 		return err
 	}
 	_, err = c.getResponse("PUT", fmt.Sprintf("/repos/%s/%s/collaborators/%s", user, repo, collaborator), nil, bytes.NewReader(body))
+	return err
+}
+
+func (c *Client) DeleteCollaborator(user, repo, collaborator string) error {
+	_, err := c.getResponse("DELETE", fmt.Sprintf("/repos/%s/%s/collaborators/%s", user, repo, collaborator), nil, nil)
+	return err
+}
+
+func (c *Client) IsCollaborator(user, repo, collaborator string) error {
+	_, err := c.getResponse("GET", fmt.Sprintf("/repos/%s/%s/collaborators/%s", user, repo, collaborator), nil, nil)
 	return err
 }
