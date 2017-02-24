bff5a78f77210a80bbcf51930e04fe6958d9cd51
Unknwon <u@gogs.io>
2017-02-24 13:56:02 -0500

    repo: able to add organization member as repository collaborator

--- gogs.go
+++ gogs.go
@@ -16,7 +16,7 @@ import (
 	"github.com/gogits/gogs/modules/setting"
 )
 
-const APP_VER = "0.9.168.0223 / 0.10 RC"
+const APP_VER = "0.9.169.0224 / 0.10 RC"
 
 func init() {
 	setting.AppVer = APP_VER
