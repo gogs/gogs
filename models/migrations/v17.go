4b07ec4717e4f6bba3627fbc8d9a7b9653e421fa
Kyle McCullough <kylemcc@gmail.com>
2017-03-29 18:02:07 -0500

    migration.v17: handle mysql error (#4361)

--- models/migrations/v17.go
+++ models/migrations/v17.go
@@ -12,7 +12,7 @@ import (
 
 func removeInvalidProtectBranchWhitelist(x *xorm.Engine) error {
 	_, err := x.Exec("DELETE FROM protect_branch_whitelist WHERE protect_branch_id = 0")
-	if err != nil && strings.Contains(err.Error(), "no such table") {
+	if err != nil && (strings.Contains(err.Error(), "no such table") || strings.Contains(err.Error(), "doesn't exist")) {
 		return nil
 	}
 	return err
