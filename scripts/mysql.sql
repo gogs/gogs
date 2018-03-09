880c989a3e3c45923ebbb8a26e4463ff45b3a102
m2nlight <wingingbob@gmail.com>
2018-03-10 04:44:07 +0800

    models: add ROW_FORMAT=DYNAMIC when creating table for supports mysql 5.6 (#4913)
    * Add ROW_FORMAT=DYNAMIC when creating table for supports mysql 5.6 (fix #4894)
    
    * Update models.go
    
    Rename NewEngine2 to NewEngineWithParams
    
    * Rename NewEngine2 to NewEngineWithParams
    
    Please update vendor when https://github.com/go-xorm/xorm/pull/798 closed.
    
    * Update dialect_mysql.go
    
    * Update dialect_mysql.go
    
    * Update models.go

--- scripts/mysql.sql
+++ scripts/mysql.sql
@@ -1,2 +1,5 @@
+SET GLOBAL innodb_file_per_table = ON,
+           innodb_file_format = Barracuda,
+           innodb_large_prefix = ON;
 DROP DATABASE IF EXISTS gogs;
 CREATE DATABASE IF NOT EXISTS gogs CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
