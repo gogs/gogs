17d789a7856c1c62f354b2f6488952b62a7bad1b
Unknwon <u@gogs.io>
2018-03-30 03:52:23 -0400

    pkg/mailer: support custom mail subject prefix (#4524)

--- gogs.go
+++ gogs.go
@@ -16,7 +16,7 @@ import (
 	"github.com/gogits/gogs/pkg/setting"
 )
 
-const APP_VER = "0.11.42.0330"
+const APP_VER = "0.11.43.0330"
 
 func init() {
 	setting.AppVer = APP_VER
--- pkg/mailer/mailer.go
+++ pkg/mailer/mailer.go
@@ -34,7 +34,7 @@ func NewMessageFrom(to []string, from, subject, htmlBody string) *Message {
 	msg := gomail.NewMessage()
 	msg.SetHeader("From", from)
 	msg.SetHeader("To", to...)
-	msg.SetHeader("Subject", subject)
+	msg.SetHeader("Subject", setting.MailService.SubjectPrefix+subject)
 	msg.SetDateHeader("Date", time.Now())
 
 	contentType := "text/html"
