05edcde6c90941dec2f80bc178394d249ba6813e
Unknwon <u@gogs.io>
2018-05-21 14:45:47 +0800

    routes/user/auth: improve coding style

--- models/user_cache.go
+++ models/user_cache.go
@@ -4,6 +4,11 @@
 
 package models
 
+// MailResendCacheKey returns key used for cache mail resend.
+func (u *User) MailResendCacheKey() string {
+	return "MailResend_" + u.IDStr()
+}
+
 // TwoFactorCacheKey returns key used for cache two factor passcode.
 // e.g. TwoFactor_1_012664
 func (u *User) TwoFactorCacheKey(passcode string) string {
