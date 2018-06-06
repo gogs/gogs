c7d06f66b16ff3b31c6d4de6f0a0cc49fb6bb40c
Alexey Terentyev <axifnx@gmail.com>
2018-06-06 16:32:54 +0300

    routes/repo: added deletion of an empty line at the end of file (#5261) (#5270)
    * Added deletion of an empty line at the end of file (#5261)
    
    Signed-off-by: Alexey Terentyev <axifnx@gmail.com>
    
    * Fix after feedback
    
    Signed-off-by: Alexey Terentyev <axifnx@gmail.com>

--- routes/repo/view.go
+++ routes/repo/view.go
@@ -185,6 +185,10 @@ func renderFile(c *context.Context, entry *git.TreeEntry, treeLink, rawLink stri
 
 			var output bytes.Buffer
 			lines := strings.Split(fileContent, "\n")
+			// Remove blank line at the end of file
+			if len(lines) > 0 && len(lines[len(lines)-1])==0 {
+				lines = lines[:len(lines)-1]
+			}
 			for index, line := range lines {
 				output.WriteString(fmt.Sprintf(`<li class="L%d" rel="L%d">%s</li>`, index+1, index+1, gotemplate.HTMLEscapeString(strings.TrimRight(line, "\r"))) + "\n")
 			}
