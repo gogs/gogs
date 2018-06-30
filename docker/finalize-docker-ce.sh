6400906380c482444d14320476effdbace7e2e7d
Michael Li <alimy@gility.net>
2018-06-30 03:20:00 +0000

    add new Dockerfile.docker-ce for docker-ce(>=v17.06) to  build docker image

--- docker/finalize-docker-ce.sh
+++ docker/finalize-docker-ce.sh
@@ -4,11 +4,12 @@
 set -x
 set -e
 
-# Move to final place
-mv /app/gogs/build/gogs /app/gogs/
+# Create git user for Gogs
+addgroup -S git
+adduser -G git -H -D -g 'Gogs Git User' git -h /data/git -s /bin/bash && usermod -p '*' git && passwd -u git
+echo "export GOGS_CUSTOM=${GOGS_CUSTOM}" >> /etc/profile
 
 # Final cleaning
-rm -rf /app/gogs/build
 rm /app/gogs/docker/build.sh
 rm /app/gogs/docker/build-go.sh
 rm /app/gogs/docker/finalize.sh
@@ -16,5 +17,3 @@ rm /app/gogs/docker/finalize-docker-ce.sh
 rm /app/gogs/docker/nsswitch.conf
 rm /app/gogs/docker/README.md
 
-rm -rf /tmp/go
-rm -rf /usr/local/go
