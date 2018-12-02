7c4b906bbb06352cc3f62e3f6c832008714e2be0
Unknwon <u@gogs.io>
2018-12-02 12:55:05 -0500

    api/repo: support edit repository issue tracker (gogs/go-gogs-client#94)

--- routes/api/v1/api.go
+++ routes/api/v1/api.go
@@ -293,6 +293,8 @@ func RegisterRoutes(m *macaron.Macaron) {
 						Patch(reqRepoWriter(), bind(api.EditMilestoneOption{}), repo.EditMilestone).
 						Delete(reqRepoWriter(), repo.DeleteMilestone)
 				})
+
+				m.Patch("/issue-tracker", bind(api.EditIssueTrackerOption{}), repo.IssueTracker)
 				m.Post("/mirror-sync", repo.MirrorSync)
 				m.Get("/editorconfig/:filename", context.RepoRef(), repo.GetEditorconfig)
 			}, repoAssignment())
