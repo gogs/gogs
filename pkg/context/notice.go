08a53e5ecaddf24a71775c85d75d7efc8806e924
unknwon <u@gogs.io>
2019-07-28 16:08:00 -0700

    pkg/context/notice: use path.Join to concatenate path

--- pkg/context/notice.go
+++ pkg/context/notice.go
@@ -19,7 +19,7 @@ import (
 // renderNoticeBanner checks if a notice banner file exists and loads the message to display
 // on all pages.
 func (c *Context) renderNoticeBanner() {
-	fpath := path.Join(setting.CustomPath, "notice/banner.md")
+	fpath := path.Join(setting.CustomPath, "notice", "banner.md")
 	if !com.IsExist(fpath) {
 		return
 	}
