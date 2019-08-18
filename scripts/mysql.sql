76dae972f3ff9a1ec3361fe4a144b7da9cecb5c4
Shiki-H <25473496+Shiki-H@users.noreply.github.com>
2019-08-18 19:06:48 -0400

    scripts/mysql: fix compatibility issue (#5786)

--- scripts/mysql.sql
+++ scripts/mysql.sql
@@ -1,5 +1,11 @@
-SET GLOBAL innodb_file_per_table = ON,
-           innodb_file_format = Barracuda,
-           innodb_large_prefix = ON;
+SET @s = IF(version() < 8 OR version() LIKE '%MariaDB%', 
+            'SET GLOBAL innodb_file_per_table = ON, 
+                        innodb_file_format = Barracuda, 
+                        innodb_large_prefix = ON;', 
+            'SET GLOBAL innodb_file_per_table = ON;');
+PREPARE stmt1 FROM @s;
+EXECUTE stmt1; 
+
 DROP DATABASE IF EXISTS gogs;
 CREATE DATABASE IF NOT EXISTS gogs CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
