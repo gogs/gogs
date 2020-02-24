aa658b6f09c3811edfbbc901fd28a84aff4d5071
Bharat Nallan <bharatnc@gmail.com>
2020-02-23 21:31:32 -0800

    add basic git repository tree api (#5934)
    This PR adds the tree  api endpoint to gogs api:
    `GET/repos/:owner/:repo/git/trees/:tree_sha`
    
    This new api endpoint that is being added is in conformance to
    the GitHub REST API v3 specification. Documentation can be found
    here: developer.github.com/v3/git/trees/#get-a-tree
    
    For a given user, repo and sha value, this api (currently) returns
    a single tree using the SHA1 value for that tree.
    
    - Recursive implementation is yet to be implemented.
    - Creating a Tree using POST is yet to be implemented.
    
    Example curl:
    
    ```
    
    l curl -H "Authorization: token REDACTED" http://localhost:3000/api/v1/repos/root/testrepo/git/trees/c59441ded1549b149def0d4c54594d31a7f3718f -X GET  | jq .
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100   940  100   940    0     0  12034      0 --:--:-- --:--:-- --:--:-- 12051
    
    [
      {
        "sha": "c59441ded1549b149def0d4c54594d31a7f3718f",
        "tree": [
          {
            "mode": "120000",
            "path": "/home/bharatnc/gogs-repositories/root/testrepo.git",
            "sha": "472ac2361b65136b393d652de25341e2ea44f299",
            "size": 1077,
            "type": "blob",
            "url": "http://localhost:3000/api/v1/repos/root/testrepo/git/trees/472ac2361b65136b393d652de25341e2ea44f299"
          },
          {
            "mode": "120000",
            "path": "/home/bharatnc/gogs-repositories/root/testrepo.git",
            "sha": "70fcb456d436f08462602f26df6fb7e167e7a916",
            "size": 12,
            "type": "blob",
            "url": "http://localhost:3000/api/v1/repos/root/testrepo/git/trees/70fcb456d436f08462602f26df6fb7e167e7a916"
          },
          {
            "mode": "120000",
            "path": "/home/bharatnc/gogs-repositories/root/testrepo.git",
            "sha": "092c58d4b63df5779a4d020b1fdbb762421bbb4f",
            "size": 380,
            "type": "blob",
            "url": "http://localhost:3000/api/v1/repos/root/testrepo/git/trees/092c58d4b63df5779a4d020b1fdbb762421bbb4f"
          }
        ],
        "url": "http://localhost:3000/api/v1/repos/root/testrepo/git/trees/c59441ded1549b149def0d4c54594d31a7f3718f"
      }
    ]
    
    ```

---
+++ internal/route/api/v1/repo/tree.go
@@ -0,0 +1,79 @@
+package repo
+
+import (
+	"fmt"
+
+	"github.com/gogs/git-module"
+
+	"gogs.io/gogs/internal/context"
+	"gogs.io/gogs/internal/db"
+)
+
+type repoGitTreeResponse struct {
+	Sha  string         `json:"sha"`
+	URL  string         `json:"url"`
+	Tree []*repoGitTree `json:"tree"`
+}
+
+type repoGitTree struct {
+	Path string `json:"path"`
+	Mode string `json:"mode"`
+	Type string `json:"type"`
+	Size int64  `json:"size,omitempty"`
+	Sha  string `json:"sha"`
+	URL  string `json:"url"`
+}
+
+func GetRepoGitTree(c *context.APIContext) {
+	repoPath := db.RepoPath(c.Params(":username"), c.Params(":reponame"))
+	gitTree, err := c.Repo.GitRepo.GetTree(c.Params(":sha"))
+	if err != nil {
+		c.NotFoundOrServerError("GetRepoTree", git.IsErrNotExist, err)
+		return
+	}
+	entries, err := gitTree.ListEntries()
+	if err != nil {
+		c.ServerError("GetRepoTree", err)
+		return
+	}
+	var children []*repoGitTree
+	var mode string
+	templateURL := `%s/repos/%s/%s/git/trees`
+	templateURL = fmt.Sprintf(templateURL, c.BaseURL, c.Params(":username"), c.Params(":reponame"))
+	if entries == nil {
+		res := &repoGitTreeResponse{
+			Sha: c.Params(":sha"),
+			URL: fmt.Sprintf(templateURL+"/%s", c.Params(":sha")),
+		}
+		c.JSONSuccess(res)
+	}
+	for _, entry := range entries {
+		switch entry.Type {
+		case git.ObjectCommit:
+			mode = "160000"
+		case git.ObjectTree:
+			mode = "040000"
+		case git.ObjectBlob:
+			mode = "120000"
+		case git.ObjectTag:
+			mode = "100644"
+		default:
+			mode = ""
+		}
+		children = append(children, &repoGitTree{
+			Path: repoPath,
+			Mode: mode,
+			Type: string(entry.Type),
+			Size: entry.Size(),
+			Sha:  entry.ID.String(),
+			URL:  fmt.Sprintf(templateURL+"/%s", entry.ID.String()),
+		})
+	}
+	results := &repoGitTreeResponse{
+		Sha:  c.Params(":sha"),
+		URL:  fmt.Sprintf(templateURL+"/%s", c.Params(":sha")),
+		Tree: children,
+	}
+
+	c.JSONSuccess(results)
+}
