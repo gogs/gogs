384d122f3887915257a0872e2857b4131a516fa7
ᴜɴᴋɴᴡᴏɴ <u@gogs.io>
2020-03-08 03:45:59 +0800

    gitutil: enhance infer submodule UR

--- internal/gitutil/submodule.go
+++ internal/gitutil/submodule.go
@@ -6,14 +6,43 @@ package gitutil
 
 import (
 	"fmt"
+	"net/url"
 	"strings"
 
 	"github.com/gogs/git-module"
+
+	"gogs.io/gogs/internal/lazyregexp"
 )
+
+var scpSyntax = lazyregexp.New(`^([a-zA-Z0-9_]+@)?([a-zA-Z0-9._-]+):(.*)$`)
 
 // InferSubmoduleURL returns the inferred external URL of the submodule at best effort.
 func InferSubmoduleURL(mod *git.Submodule) string {
-	urlStr := mod.URL()
-	urlStr = strings.TrimSuffix(urlStr, ".git")
-	return fmt.Sprintf("%s/commit/%s", urlStr, mod.Commit())
+	raw := strings.TrimSuffix(mod.URL, "/")
+	raw = strings.TrimSuffix(raw, ".git")
+
+	parsed, err := url.Parse(raw)
+	if err != nil {
+		// Try parse as SCP syntax again
+		match := scpSyntax.FindAllStringSubmatch(raw, -1)
+		if len(match) == 0 {
+			return mod.URL
+		}
+		parsed = &url.URL{
+			Scheme: "http",
+			Host:   match[0][2],
+			Path:   match[0][3],
+		}
+	}
+
+	switch parsed.Scheme {
+	case "http", "https":
+		raw = parsed.String()
+	case "ssh":
+		raw = fmt.Sprintf("http://%s%s", parsed.Host, parsed.Path)
+	default:
+		return raw
+	}
+
+	return fmt.Sprintf("%s/commit/%s", raw, mod.Commit)
 }
