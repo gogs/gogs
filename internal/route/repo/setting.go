3e055e329cf93eb5de77562d7795240808d31c08
alpencrossi <62932811+alpencrossi@users.noreply.github.com>
2020-04-08 22:13:50 +0200

    repo: disable public access option when using external wiki and issue tracker (#6081)

--- internal/route/repo/setting.go
+++ internal/route/repo/setting.go
@@ -156,6 +156,13 @@ func SettingsPost(c *context.Context, f form.RepoSetting) {
 		repo.PullsIgnoreWhitespace = f.PullsIgnoreWhitespace
 		repo.PullsAllowRebase = f.PullsAllowRebase
 
+		if !repo.EnableWiki || repo.EnableExternalWiki {
+			repo.AllowPublicWiki = false
+		}
+		if !repo.EnableIssues || repo.EnableExternalTracker {
+			repo.AllowPublicIssues = false
+		}
+
 		if err := db.UpdateRepository(repo, false); err != nil {
 			c.Error(err, "update repository")
 			return
