28d74a98444c372ef2882c368559c8e4784a7f6f
eightpigs <eightpigs@outlook.com>
2020-04-15 19:42:32 +0800

    docker: fix the bug of using root to create /data/ directory (#6106)

--- docker/start.sh
+++ docker/start.sh
@@ -33,16 +33,17 @@ create_volume_subfolder() {
     # Create VOLUME subfolder
     for f in /data/gogs/data /data/gogs/conf /data/gogs/log /data/git /data/ssh; do
         if ! test -d $f; then
-            mkdir -p $f
+            gosu $USER mkdir -p $f
         fi
     done
 }
 
 setids() {
+    export USER=git
     PUID=${PUID:-1000}
     PGID=${PGID:-1000}
-    groupmod -o -g "$PGID" git
-    usermod -o -u "$PUID" git
+    groupmod -o -g "$PGID" $USER
+    usermod -o -u "$PUID" $USER
 }
 
 setids
