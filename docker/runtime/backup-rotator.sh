560bb7e4e4d7ac85b1a7ebb2a424422717026a81
xZero707 <xzero@elite7hackers.net>
2020-04-19 14:11:55 +0200

    Add docblock

--- docker/runtime/backup-rotator.sh
+++ docker/runtime/backup-rotator.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+# This is very simple, yet effective backup rotation script
+# Using find command, all files that are older than BACKUP_RETENTION_DAYS are accumulated and deleted using rm
 main() {
 	BACKUP_PATH="${1:-}"
 	BACKUP_RETENTION_DAYS="${2:-}"
