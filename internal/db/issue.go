c98a55b0215fa45024e0612a1683592b86bb1835
ᴜɴᴋɴᴡᴏɴ <u@gogs.io>
2020-08-17 21:09:22 +0800

    organization: limit showing at most 12 members in profile page (#6254)

--- internal/db/issue.go
+++ internal/db/issue.go
@@ -1173,7 +1173,7 @@ func updateIssueMentions(e Engine, issueID int64, mentions []string) error {
 		}
 
 		memberIDs := make([]int64, 0, user.NumMembers)
-		orgUsers, err := getOrgUsersByOrgID(e, user.ID)
+		orgUsers, err := getOrgUsersByOrgID(e, user.ID, 0)
 		if err != nil {
 			return fmt.Errorf("getOrgUsersByOrgID [%d]: %v", user.ID, err)
 		}
