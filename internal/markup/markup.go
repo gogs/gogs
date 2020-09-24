d5e3292ddf85ba4ce7f45ea47d50c2a8cf8b4a7f
Zhukov Roman <zhukov.roman@gmail.com>
2020-09-24 13:18:44 +0300

    Remove branch prefix from SHA URL

--- internal/markup/markup.go
+++ internal/markup/markup.go
@@ -145,6 +145,9 @@ func RenderSha1CurrentPattern(rawBytes []byte, urlPrefix string) []byte {
 		if com.StrTo(m).MustInt() > 0 {
 			return m
 		}
+		if cutPosition := strings.LastIndex(urlPrefix, "/src/"); cutPosition > 0 {
+			urlPrefix = urlPrefix[:cutPosition]
+		}
 		return fmt.Sprintf(`<a href="%s/commit/%s"><code>%s</code></a>`, urlPrefix, m, tool.ShortSHA1(string(m)))
 	}))
 }
--- internal/markup/markup_test.go
+++ internal/markup/markup_test.go
@@ -215,3 +215,37 @@ func Test_RenderIssueIndexPattern(t *testing.T) {
 		})
 	})
 }
+
+func TestRenderSha1CurrentPattern(t *testing.T) {
+	tests := []struct {
+		desc   string
+		input  string
+		prefix string
+		expVal string
+	}{
+		{
+			desc:   "Full SHA (40 symbols)",
+			input:  "ad8ced4f57d9068cb2874557245be3c7f341149d",
+			prefix: "/owner/repo/src/branch",
+			expVal: "<a href=\"/owner/repo/commit/ad8ced4f57d9068cb2874557245be3c7f341149d\"><code>ad8ced4f57</code></a>",
+		},
+		{
+			desc:   "Short SHA (8 symbols)",
+			input:  "ad8ced4f",
+			prefix: "/owner/repo/src/branch",
+			expVal: "<a href=\"/owner/repo/commit/ad8ced4f\"><code>ad8ced4f</code></a>",
+		},
+		{
+			desc:   "9 digits",
+			input:  "123456789",
+			prefix: "/owner/repo/src/branch",
+			expVal: "123456789",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			assert.Equal(t, test.expVal, string(RenderSha1CurrentPattern([]byte(test.input), test.prefix)))
+		})
+	}
+}
