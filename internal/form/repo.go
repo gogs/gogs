ded79b2a3e8dea4b31c8a1680d103ad80e2d4c89
amoohashem <amoo.hashem.mehraban@gmail.com>
2021-02-16 03:58:19 +0330

    Remove restriction for repository website url length

--- internal/form/repo.go
+++ internal/form/repo.go
@@ -89,7 +89,7 @@ func (f MigrateRepo) ParseRemoteAddr(user *db.User) (string, error) {
 type RepoSetting struct {
 	RepoName      string `binding:"Required;AlphaDashDot;MaxSize(100)"`
 	Description   string `binding:"MaxSize(512)"`
-	Website       string `binding:"Url;MaxSize(100)"`
+	Website       string `binding:"Url"`
 	Branch        string
 	Interval      int
 	MirrorAddress string
