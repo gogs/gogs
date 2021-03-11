7e87548a69945b7dfe284cd73fa33d8b6f924e6a
rholovchak <holovchak1805@gmail.com>
2021-03-11 20:36:23 +0200

    b

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Stops the process if something fails
 set -xe
+echo $GOPATH
+ls $GOPATH
 go get
 # create the application binary that eb uses
 GOOS=linux GOARCH=amd64 go build -o bin/application application.go
