b561c323409219c828c0a35caf7ee5d73df1da0d
rholovchak <holovchak1805@gmail.com>
2021-03-11 20:44:24 +0200

    b

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ sudo rm /var/app/current/go.*
 echo $GOPATH
 ls $GOPATH
 go get
+mkdir -p /var/app/current/bin/custom/conf
 # create the application binary that eb uses
 GOOS=linux GOARCH=amd64 go build -o bin/application application.go
+cp custom/conf/app.ini /var/app/current/bin/custom/conf/app.ini
