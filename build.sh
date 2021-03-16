277bfeb8e78bdecd08e9441d5208b658eee15e05
rholovchak <68376355+rholovchak@users.noreply.github.com>
2021-03-16 12:03:45 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 # Stops the process if something fails
 set -xe
-sudo rm /var/app/current/go.*
 echo $GOPATH
 ls $GOPATH
 go get
 # create the application binary that eb uses
 GOOS=linux GOARCH=amd64 go build -o bin/application application.go
-ls
-sudo cp -R "custom/conf/app.ini" /var/app/current/bin/
