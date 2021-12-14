e8ea82bba678e93c0e613fac889a3f2031ab02a9
Joe Chen <jc@unknwon.io>
2021-12-14 14:35:15 +0800

    Apply suggestions from code review

--- internal/route/api/v1/repo/tag.go
+++ internal/route/api/v1/repo/tag.go
@@ -1,3 +1,7 @@
+// Copyright 2021 The Gogs Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 package repo
 
 import (
