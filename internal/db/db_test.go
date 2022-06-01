6534427b3b10d73e08b87bb0b79bbe799cd78c6c
Joe Chen <jc@unknwon.io>
2022-06-01 22:17:21 +0800

    fix compile error

--- internal/db/db_test.go
+++ internal/db/db_test.go
@@ -140,7 +140,7 @@ func Test_parseDSN(t *testing.T) {
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			dsn, err := parseDSN(test.opts)
+			dsn, err := newDSN(test.opts)
 			if err != nil {
 				t.Fatal(err)
 			}
