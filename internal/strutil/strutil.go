c4cea347a383ddb844f147eaafe58c2c1ed3bd94
Joe Chen <jc@unknwon.io>
2022-06-13 21:58:57 +0800

    format docstring

--- internal/strutil/strutil.go
+++ internal/strutil/strutil.go
@@ -45,8 +45,9 @@ func RandomChars(n int) (string, error) {
 	return string(buffer), nil
 }
 
-// Ellipsis returns a truncated string and appends "..." to the end of the string if the
-// string length is larger than the threshold. Otherwise, the original string is returned.
+// Ellipsis returns a truncated string and appends "..." to the end of the
+// string if the string length is larger than the threshold. Otherwise, the
+// original string is returned.
 func Ellipsis(str string, threshold int) string {
 	if len(str) <= threshold || threshold < 0 {
 		return str
