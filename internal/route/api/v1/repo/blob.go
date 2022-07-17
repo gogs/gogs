77662fc3ae240a3d9e16c468f478dc4b381e1b6f
Joe Chen <jc@unknwon.io>
2022-07-17 15:01:02 +0800

    Apply suggestions from code review

--- internal/route/api/v1/repo/blob.go
+++ internal/route/api/v1/repo/blob.go
@@ -12,7 +12,7 @@ import (
 )
 
 func RepoGitBlob(c *context.APIContext) {
-	gitRepo, err := git.Open(repoutil.RepositoryPath(c.Params("username"), c.Params("reponame")))
+	gitRepo, err := git.Open(c.Repo.Repository.RepoPath())
 	if err != nil {
 		c.Error(err, "open repository")
 		return
@@ -22,26 +22,28 @@ func RepoGitBlob(c *context.APIContext) {
 	blob, err := gitRepo.CatFileBlob(sha)
 	if err != nil {
 		c.NotFoundOrError(gitutil.NewError(err), "get blob")
+		return
 	}
 
 	type repoGitBlob struct {
 		Content  string `json:"content"`
 		Encoding string `json:"encoding"`
 		URL      string `json:"url"`
-		Sha      string `json:"sha"`
+		SHA      string `json:"sha"`
 		Size     int64  `json:"size"`
 	}
 
 	content, err := blob.Blob().Bytes()
 	if err != nil {
 		c.NotFoundOrError(gitutil.NewError(err), "get blob content")
+		return
 	}
 
 	c.JSONSuccess(&repoGitBlob{
 		Content:  base64.StdEncoding.EncodeToString(content),
 		Encoding: "base64",
 		URL:      fmt.Sprintf("%s/repos/%s/%s/git/blobs/%s", c.BaseURL, c.Params(":username"), c.Params(":reponame"), sha),
-		Sha:      sha,
+		SHA:      sha,
 		Size:     blob.Size(),
 	})
 }
