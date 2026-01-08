e4e624ca802929ab6ae3b38cb8d67b4d558f5fec
Joe Chen <jc@unknwon.io>
2026-01-08 17:52:38 -0500

    chore: remove all MIT license file headers

--- gogs.go
+++ gogs.go
@@ -1,9 +1,5 @@
 //go:build go1.18
 
-// Copyright 2014 The Gogs Authors. All rights reserved.
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE file.
-
 // Gogs is a painless self-hosted Git Service.
 package main
 
--- internal/conf/static_minwinsvc.go
+++ internal/conf/static_minwinsvc.go
@@ -1,9 +1,5 @@
 //go:build minwinsvc
 
-// Copyright 2015 The Gogs Authors. All rights reserved.
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE file.
-
 package conf
 
 import (
