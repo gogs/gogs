1bbc36149a371b91d9db8a672728a3ce6dbff003
ᴊᴏᴇ ᴄʜᴇɴ <jc@unknwon.io>
2026-01-22 11:00:27 -0500

    wiki: sanitize old wiki page name when editing (#8099)

---
+++ internal/database/wiki_test.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestToWikiPageName(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{input: "Home", want: "Home"},
+		{input: "../../../../tmp/target_file", want: "tmp target_file"},
+		{input: "..\\..\\..\\..\\tmp\\target_file", want: "tmp target_file"},
+		{input: "A/B", want: "A B"},
+		{input: "../pwn", want: "pwn"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.input, func(t *testing.T) {
+			got := ToWikiPageName(test.input)
+			require.Equal(t, test.want, got)
+		})
+	}
+}
