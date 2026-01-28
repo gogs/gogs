df3d945a2ccd93aa49db9d42e9e0ec6a1dd32b24
pikomonde <32364823+pikomonde@users.noreply.github.com>
2026-01-28 23:36:03 +0700

    config: validate and print warnings for invalid options (#7705)
    Co-authored-by: Joe Chen <jc@unknwon.io>

--- internal/conf/conf.go
+++ internal/conf/conf.go
@@ -346,6 +346,9 @@ func Init(customConf string) error {
 	LFS.ObjectsPath = ensureAbs(LFS.ObjectsPath)
 
 	handleDeprecated()
+	for _, warning := range checkInvalidOptions(File) {
+		log.Warn("%s", warning)
+	}
 
 	if err = File.Section("cache").MapTo(&Cache); err != nil {
 		return errors.Wrap(err, "mapping [cache] section")
