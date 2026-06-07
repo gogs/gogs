79fdabdc070ffbbd2763bc7ce0e38c6e634ab82f
ᴊᴏᴇ ᴄʜᴇɴ <jc@unknwon.io>
2026-06-06 23:33:30 -0400

    security: require token auth for org metadata and team list (#8336)
    (cherry picked from commit 2ebc0e27069deade992219e10a89fbc44bec8bb9)

--- internal/route/api/v1/api.go
+++ internal/route/api/v1/api.go
@@ -389,7 +389,7 @@ func RegisterRoutes(m *macaron.Macaron) {
 				Get(org.Get).
 				Patch(bind(api.EditOrgOption{}), org.Edit)
 			m.Get("/teams", org.ListTeams)
-		}, orgAssignment(true))
+		}, reqToken(), orgAssignment(true))
 
 		m.Group("/admin", func() {
 			m.Group("/users", func() {
